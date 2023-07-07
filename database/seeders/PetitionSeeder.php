c2b5bf32351277e696d81adce9bac976ef70d9be
rifgialhkm <rifgi115@gmail.com>
2023-07-08 01:07:29 +0700

    Initiate project | Creating model, migration, seeder, factory, controller, and resource | Creating the database | Creating fake data

---
+++ database/seeders/PetitionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Petition;
+use Illuminate\Database\Seeder;
+
+class PetitionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Petition::factory()->times(50)->create();
+        // then run php artisan db:seed --class=PetitionSeeder
+    }
+}
